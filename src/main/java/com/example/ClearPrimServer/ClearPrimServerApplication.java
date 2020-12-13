c042aef5c09d358c418b9ee095d71e70bea483af
Dmitriy <rabowvdk125@gmail.com>
2020-12-13 23:53:06 +1000

    Initial commit

---
+++ src/main/java/com/example/ClearPrimServer/ClearPrimServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClearPrimServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearPrimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearPrimServerApplication.class, args);
+	}
+
+}
